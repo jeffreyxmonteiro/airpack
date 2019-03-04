79cedaa2d1297e523562e1ca61479aa63c261d9a
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-03-04 19:25:08 +0900

    Initial Seeding Code.:

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,250 @@
+require 'faker'
+Traveler.destroy_all
+Packer.destroy_all
+Booking.destroy_all
+Item.destroy_all
+Pack.destroy_all
+Cart.destroy_all
+
+puts "Creating seeds."
+
+puts "Creating Traveler Seeds"
+
+50.times do
+  Traveler.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    photo_url: AVATARIMAGES.sample,
+    biography: "What's up! Huge travel junkie, originally from #{Faker::Address.country}! Some of my other interests are reading #{Faker::DcComics.hero} comics and eating #{Faker::Dessert.variety}."
+    )
+end
+
+puts "Finished Creating Traveler Seeds"
+
+puts "Creating Packer Seeds"
+
+50.times do
+  Packer.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    photo_url: USERIMAGES.sample,
+    biography: "Living that good life in Tokyo! My other interests are reading #{Faker::DcComics.hero} comics and eating #{Faker::Dessert.variety}."
+    location: TOKYOLOCATIONS.sample
+    )
+end
+
+puts "Finished Packer Traveler Seeds"
+
+puts "Creating Initial Packs Seeds"
+
+puts "Finished Creating Initial Packs Seeds"
+
+puts "Creating Items Seeds"
+
+puts "Finished Creating Items Seeds"
+
+puts "Creating Completed Packs Seeds"
+
+puts "Finished Creating Completed Packs Seeds"
+
+puts "Creating Bookings Seeds"
+
+puts "Finished Bookings Packs Seeds"
+
+puts "Finished creating all seeds :D"
+
+AVATARIMAGES = [
+                "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                "https://images.pexels.com/photos/769772/pexels-photo-769772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                "https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                "https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                "https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                "https://images.pexels.com/photos/1139743/pexels-photo-1139743.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                "https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                "https://images.pexels.com/photos/1024311/pexels-photo-1024311.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                "https://images.pexels.com/photos/227294/pexels-photo-227294.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                ]
+
+USERIMAGES = [
+              "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              "https://images.pexels.com/photos/555790/pexels-photo-555790.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              "https://images.pexels.com/photos/247206/pexels-photo-247206.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              "https://images.pexels.com/photos/245239/pexels-photo-245239.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              "https://images.pexels.com/photos/965324/pexels-photo-965324.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              "https://images.pexels.com/photos/1689731/pexels-photo-1689731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              "https://images.pexels.com/photos/1722198/pexels-photo-1722198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              "https://images.pexels.com/photos/1001180/pexels-photo-1001180.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              ]
+
+TOKYOLOCATIONS = [
+              "Shibuya, Tokyo",
+              "Ginza, Tokyo",
+              "Shinjuku, Tokyo",
+              "Meguro, Tokyo",
+              "Roppongi, Tokyo",
+              "Minato, Tokyo",
+              "Gotanda, Tokyo",
+              "Shinagawa, Tokyo",
+              "Shin-Okubo, Tokyo",
+              "Chiba, Tokyo"
+              ]
+STYLES = [
+'Style',
+'Jock',
+'Prepster',
+'Hipster',
+'Corporate',
+'Hip hop',
+'Outdoorsy',
+'Rockstar',
+'Vintage',
+]
+
+SIZES = [
+'XS',
+'S',
+'M',
+'L',
+'XL',
+'XXL',
+'XXXL'
+]
+
+ITEMCATEGORY = [
+"Casual Shirt Short Sleeve",
+"Casual Shirt Long Sleeve",
+"Casual Button Down",
+"Blazer",
+"Formal Button Down",
+"Hats",
+"Casual Pants",
+"Formal Pants",
+"Gloves",
+"Scarf",
+"Shoes",
+"Belt",
+"Formal Shorts",
+"Athletic Shorts",
+"Atheltic Top",
+"Sweater",
+"Outerwear Jacket",
+"Hoodie",
+"Bathing Suit",
+"New Toiletries",
+"New Underwear",
+"New Socks",
+"Miscellaneous :)"
+]
+
+TOPSIMAGES = [
+              "https://im.uniqlo.com/images/jp/pc/img/feature/uq/topscollection/men/190105-19SS_tops_M_link_knitcollection.jpg",
+              "https://im.uniqlo.com/images/jp/pc/img/feature/uq/topscollection/men/190111-19SS_tops_M_link_sweat.jpg",
+              "https://im.uniqlo.com/images/jp/pc/img/feature/uq/topscollection/men/190105-19SS_tops_M_link_cardigan.jpg",
+              "https://im.uniqlo.com/images/jp/pc/img/feature/uq/topscollection/men/190215-19SS_tops_M_link_dressshirts.jpg",
+              "https://im.uniqlo.com/images/jp/pc/img/feature/uq/topscollection/men/190215-19SS_tops_M_link_casualshirts.jpg",
+              "https://im.uniqlo.com/images/jp/pc/img/feature/uq/topscollection/men/190105-19SS_tops_M_link_tshirt.jpg",
+              "https://im.uniqlo.com/images/jp/pc/img/feature/uq/topscollection/men/190208-19SS_tops_M_link_dressshirts.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/416718/item/08_416718_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/413433/item/69_413433_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/413436/item/03_413436_middles.jpg"
+              ]
+
+BOTTOMSIMAGES = [
+              "https://im.uniqlo.com/images/jp/pc/goods/413156/item/68_413156_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/416454/item/56_416454_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/416976/item/69_416976_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/416458/item/33_416458_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/413154/item/09_413154_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/418170/item/31_418170_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/416455/item/06_416455_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/416457/item/08_416457_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/416459/item/06_416459_middles.jpg",
+              "https://im.uniqlo.com/images/jp/pc/goods/414654/item/64_414654_middles.jpg"
+              ]
+HATS = [
+        "https://im.uniqlo.com/images/jp/pc/goods/408922/item/43_408922_middles.jpg",
+        "https://im.uniqlo.com/images/jp/pc/goods/416496/item/34_416496_middles.jpg",
+        "https://im.uniqlo.com/images/jp/pc/goods/413074/item/56_413074_middles.jpg",
+        "https://im.uniqlo.com/images/jp/pc/goods/410857/item/69_410857_middles.jpg",
+        "https://im.uniqlo.com/images/jp/pc/goods/413075/item/35_413075_middles.jpg",
+        "https://im.uniqlo.com/images/jp/pc/goods/413076/item/62_413076_middles.jpg"
+]
+
+SCARVES = [
+        "https://im.uniqlo.com/images/jp/pc/goods/414302/item/13_414302_middles.jpg",
+        "https://im.uniqlo.com/images/jp/pc/goods/408918/item/09_408918_middles.jpg",
+        "https://im.uniqlo.com/images/jp/pc/goods/413107/item/21_413107_middles.jpg",
+        "https://im.uniqlo.com/images/jp/pc/goods/414303/item/69_414303_middles.jpg",
+        "https://im.uniqlo.com/images/jp/pc/goods/416409/item/69_416409_middles.jpg",
+        "https://im.uniqlo.com/images/jp/pc/goods/417740/item/15_417740_middles.jpg"
+]
+
+SHOES = [
+  "https://im.uniqlo.com/images/jp/pc/goods/410860/item/08_410860_middles.jpg",
+  "https://im.uniqlo.com/images/jp/pc/goods/416497/item/00_416497_middles.jpg"
+]
+
+BELTS = [
+  "https://im.uniqlo.com/images/jp/pc/goods/413069/item/09_413069_middles.jpg",
+  "https://im.uniqlo.com/images/jp/pc/goods/415573/item/37_415573_middles.jpg",
+  "https://im.uniqlo.com/images/jp/pc/goods/416101/item/38_416101_middles.jpg"
+]
+
+
+
+
+
+
+
+
+=begin
+dogs
+"https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+"https://images.pexels.com/photos/350428/pexels-photo-350428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/374906/pexels-photo-374906.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/257540/pexels-photo-257540.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/235805/pexels-photo-235805.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/164186/pexels-photo-164186.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/9080/night-garden-yellow-animal.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1591939/pexels-photo-1591939.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/97082/weimaraner-puppy-dog-snout-97082.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1124002/pexels-photo-1124002.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+
+user
+"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/555790/pexels-photo-555790.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/247206/pexels-photo-247206.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/245239/pexels-photo-245239.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/965324/pexels-photo-965324.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1689731/pexels-photo-1689731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1722198/pexels-photo-1722198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1001180/pexels-photo-1001180.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+
+
+owner
+"https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/769772/pexels-photo-769772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1139743/pexels-photo-1139743.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/1024311/pexels-photo-1024311.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+"https://images.pexels.com/photos/227294/pexels-photo-227294.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+
+=end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
