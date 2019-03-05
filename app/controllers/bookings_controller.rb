e48f289e552f776708f7560279859b92be5c42f8
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-03-05 18:23:06 +0900

    Created form and policy portion of new booking page.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @traveler = current_user
+    @booking = Booking.new
+  end
+
+  def create
+  end
 end
