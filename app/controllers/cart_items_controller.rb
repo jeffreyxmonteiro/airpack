771b9905060b69956d594a374a044d56cf85c358
Adis Puangchompoo <apuangchompoo@gmail.com>
2019-03-06 13:51:45 +0900

    working cartable item

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,16 +1,22 @@
 class CartItemsController < ApplicationController
   def create
-    # Needs an Object
     CartItem.create!(
       cart: current_traveler.cart,
-      cartable: @pack || @item # item or pack
+      cartable_type: params[:cartable_type],
+      cartable_id: params[:cartable_id]
     )
-    redirect_to packs_show_path
+    redirect_to packs_path
   end
 
   def destroy
     @cart_item = cart_item
     @cart_item.destroy
-    redirect_to packs_show_path
+    redirect_to packs_path
+  end
+
+  private
+
+  def cartable_params
+    params.permit(:cartable_type, :cartable_id)
   end
 end
