f73c401b7e2e2eaedecf62499143397d2a25a3a8
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-03-07 16:44:06 +0900

    Finalized design on traveler profile.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_085616) do
+ActiveRecord::Schema.define(version: 2019_03_07_051436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2019_03_06_085616) do
     t.string "delivery_address"
     t.string "delivery_date"
     t.string "return_deadline"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.bigint "traveler_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
