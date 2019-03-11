fa8abf202c36e14cf2665cc0caa1eb79152c110a
Adis Puangchompoo <apuangchompoo@gmail.com>
2019-03-11 11:49:58 +0900

    added logic for adding items to pack + fixed bugs

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
   resource :profile, only: [:show, :edit, :update] do
     get '/closet', to: 'profiles#closet'
   end
-  resources :packs, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :items, only: [:index, :show, :new, :create, :destroy]
-  resources :bookings, only: [:show, :new, :create, :update, :destroy]
+  resources :packs, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :items, only: :update
+  end
+  resources :bookings, only: [:show, :new, :create, :destroy]
   resources :cart_items, only: [:create, :destroy]
-  resource :cart, only: [:show]
   delete '/carts/:id', to: 'cart#clear', as: 'clear_cart'
+  resources :temp_closet_items, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
