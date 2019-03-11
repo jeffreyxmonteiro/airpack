9f7988e8fc00aebb9b41ca5d070005ad8eeb2810
Francis Abad <francisongabad@gmail.com>
2019-03-11 15:26:15 +0900

    cart drop down and icon badge now refreshes to delete

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,7 +12,7 @@ class CartItemsController < ApplicationController
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
-    redirect_back(fallback_location: packs_path)
+    render :destroy
   end
 
   private
