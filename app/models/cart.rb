fe5fe0a949f3acaa2df6cb8b27b23d89df28e84d
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-03-12 13:39:54 +0900

    Added subtotal to top of cart.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,4 +5,9 @@ class Cart < ApplicationRecord
   def clear_cart
     cart_items.each(&:destroy)
   end
+
+  def cost
+    cart_objects = cart_items.map(&:cartable)
+    return cart_objects.map(&:price).sum
+  end
 end
