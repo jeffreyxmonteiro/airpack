15e03c1af89ad83e052ff813a41c3e3f60883e9d
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-03-12 13:29:59 +0900

    Fixed delete buttons and added subtotal of cart.

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,7 +12,7 @@ class CartItemsController < ApplicationController
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
-    render :destroy
+    redirect_back(fallback_location: packs_path)
   end
 
   private
